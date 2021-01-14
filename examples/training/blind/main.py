7d3f538b4ee4b26d130c0911d0f3afd63805551c
zzx1997zzx <346645583@qq.com>
2021-01-14 18:44:54 +0800

    upload files

---
+++ examples/training/blind/main.py
@@ -0,0 +1,29 @@
+import torch
+
+from torch.autograd import Variable
+from trainer import Vrae
+from config import get_config
+import iterator
+from utils import prepare_dirs_and_logger
+
+
+def main(config):
+    '''
+    this main fun for ...
+    :param config:
+    :return:
+    '''
+    prepare_dirs_and_logger(config)
+    step = iterator.Iterator(config)
+    trainer = Vrae(config, step)
+    if config.is_train:
+        trainer.train()
+    else:
+        # trainer.test()
+        # trainer.div3()
+        # trainer.restore_temp()
+        trainer.get_train_ppl()
+
+if __name__ == "__main__":
+    config, unparsed = get_config()
+    main(config)
